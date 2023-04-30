201ee6623a7e3b8150652560f39ebb6440ab1a43
jared <jaredgold@proton.me>
2023-04-29 20:34:14 -0400

    install HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather App',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
