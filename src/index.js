48bba61a2a70659b72a92992378aa3c5049eed95
jared <jaredgold@proton.me>
2023-04-30 20:03:49 -0400

    create async func to get weather data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import './style.css';
 
-console.log("heyo");
+console.log("jaredgfff");
 console.log("spyder");
+/*
+on page load, get weather info of user IP address.
+
+display it.
+
+search input for city, enter will get that city, and display it.
+
+if no city found, error.
+*/
+
+async function getWeather(city) {
+  const weather = await fetch(`https://api.weatherapi.com/v1/current.json?key=a3f0ed6f12f14ffcaf5221928232504&q=${city}&aqi=no`);
+  const weatherData = await weather.json();
+  return console.log(weatherData);
+}
+
+getWeather('miami');
