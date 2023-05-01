894fe2205731cb06cdcb6c9e0ec7b5974bd0d79b
jared <jaredgold@proton.me>
2023-05-01 15:53:07 -0400

    pull icon/desc and implement img into dom, add correct values to dom

--- src/display.js
+++ src/display.js
@@ -1,15 +1,16 @@
 export default function injectDisplay(data) {
   const newData = Object.keys(data);
   const container = document.getElementById('container');
-  // for(let i = 1; i < newData.length; i++) {
-  //   console.log(newData[i][value])
-  // }
   newData.forEach((item) => {
+    if(item === 'currentIcon') {
+      const icon = document.createElement('img');
+      icon.setAttribute('src', data[item]);
+      container.append(icon);
+      return;
+    }
     const entry = document.createElement('div');
-    // entry.classList.add(`${data}`);
-    entry.textContent = `${item}: ${data[item]}`;
+
+    entry.textContent = `${data[item]}`;
     container.append(entry);
-    // console.log(data[item])
-    // console.log(item)
   })
 }
