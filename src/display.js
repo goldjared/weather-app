fff26348364e581523a9915de5ca71ba1eb48711
jared <jaredgold@proton.me>
2023-05-01 16:24:35 -0400

    format with prettier

--- src/display.js
+++ src/display.js
@@ -1,22 +1,21 @@
 export default function injectDisplay(data) {
   const newData = Object.keys(data);
-  const container = document.getElementById('container');
+  const container = document.getElementById("container");
   (function clearDisplay() {
-    while(container.lastElementChild) {
+    while (container.lastElementChild) {
       container.removeChild(container.lastElementChild);
     }
-  }());
+  })();
   newData.forEach((item) => {
-    if(item === 'currentIcon') {
-      const icon = document.createElement('img');
-      icon.setAttribute('src', data[item]);
+    if (item === "currentIcon") {
+      const icon = document.createElement("img");
+      icon.setAttribute("src", data[item]);
       container.append(icon);
       return;
     }
-    const entry = document.createElement('div');
+    const entry = document.createElement("div");
 
     entry.textContent = `${data[item]}`;
     container.append(entry);
-  })
+  });
 }
-
--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@ import injectDisplay from "./display";
 function processData(data) {
   const currentData = {
     location: `Location: ${data.location.name}, ${data.location.region}`,
-    currentIcon: data.current.condition.icon, 
+    currentIcon: data.current.condition.icon,
     currentCondition: data.current.condition.text,
     time: `Date/Time: ${data.location.localtime}`,
     temp: `Temperature: ${data.current.temp_f}°F`,
     humidity: `Humidity: ${data.current.humidity}`,
     updated: `Last Updated: ${data.current.last_updated}`,
-  }
+  };
   return currentData;
 }
 
 async function getWeather(city) {
   try {
-    const response= await fetch(
+    const response = await fetch(
       `https://api.weatherapi.com/v1/current.json?key=a3f0ed6f12f14ffcaf5221928232504&q=${city}&aqi=no`
     );
-    if(!response.ok) {
+    if (!response.ok) {
       throw new Error(response.statusText);
     }
     const weatherData = await response.json();
@@ -36,6 +36,6 @@ async function getWeather(city) {
   submit.addEventListener("click", (e) => {
     e.preventDefault();
     getWeather(input.value);
-    input.value = '';
+    input.value = "";
   });
 })();
